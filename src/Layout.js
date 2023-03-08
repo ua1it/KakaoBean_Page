cb28c20feec1e9693ec29caf18b006631585eb29
ua1it <hoo9909@naver.com>
2023-03-08 20:21:04 +0900

    [ADD]상단 네비게이션 바 디자인 구현_2

--- src/Layout.js
+++ src/Layout.js
@@ -1,36 +1,50 @@
-import {React, useState} from "react";
+import { React, useState } from "react";
 import { Outlet, Link } from "react-router-dom";
-import './Layout.css'
+import "@fortawesome/fontawesome-free";
+import "./Layout.css";
+import Brand from "./images/coffee-beans.png";
+import Menu from "./images/menu.png";
 
 export default function Layout() {
-
-  const [showNavbar, setShowNavbar] = useState(true);
+  const [showNavbar, setShowNavbar] = useState(false);
   const handleShowNavbar = () => {
     setShowNavbar(!showNavbar);
-  }
+  };
 
   return (
-      <nav className="navbar">
-        <div className="container">
-          <div className={`nav-elements ${showNavbar && 'active'}`}>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/vision">Vision</Link>
-              </li>
-              <li>
-                <Link to="/team">Team</Link>
-              </li>
-              <li>
-                <Link to="/custom">Custom</Link>
-              </li>
-            </ul>
-          </div>
+    <nav className="navbar">
+      <div className="container">
+        <img className="logo" src={Brand} />
+        <h1 className="navbar-logo">KakaoBean</h1>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          <i className={showNavbar ? "fas fa-times" : "fas fa-bars"}></i>
         </div>
-        <Outlet />
-      </nav>
-      
+        <div className="nav-elements">
+          <ul className={showNavbar ? "nav-elements active" : "nav-elements"}>
+            <li>
+              <a href="/" className="nav-links">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/vision" className="nav-links">
+                Vision
+              </a>
+            </li>
+            <li>
+              <a href="/team" className="nav-links">
+                Team
+              </a>
+            </li>
+            <li>
+              <a href="/custom" className="nav-links">
+                Custom
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <Outlet />
+    </nav>
   );
 }
