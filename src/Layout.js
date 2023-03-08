f8d5202b8d8349ee402d8fb3365dee207a66b4a9
ua1it <hoo9909@naver.com>
2023-03-08 10:36:33 +0900

    [ADD]상단 네비게이션 바 디자인 구현_1

--- src/Layout.js
+++ src/Layout.js
@@ -1,26 +1,36 @@
-import React from 'react'
-import { Outlet, Link } from 'react-router-dom'
+import {React, useState} from "react";
+import { Outlet, Link } from "react-router-dom";
+import './Layout.css'
 
 export default function Layout() {
+
+  const [showNavbar, setShowNavbar] = useState(true);
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar);
+  }
+
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to ="/">Home</Link>
-          </li>
-          <li>
-            <Link to ="/vision">Vision</Link>
-          </li>
-          <li>
-            <Link to ="/team">Team</Link>
-          </li>
-          <li>
-            <Link to ="/custom">Custom</Link>
-          </li>
-        </ul>
+      <nav className="navbar">
+        <div className="container">
+          <div className={`nav-elements ${showNavbar && 'active'}`}>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/vision">Vision</Link>
+              </li>
+              <li>
+                <Link to="/team">Team</Link>
+              </li>
+              <li>
+                <Link to="/custom">Custom</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <Outlet />
       </nav>
-      <Outlet/>
-    </>
-  )
+      
+  );
 }
