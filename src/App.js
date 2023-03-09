955eb6fd476c29763f12cdfe1d7e17a0a3010ca2
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2023-03-09 09:57:28 +0900

    Chore: import 위치 변경

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import './App.css';
+import "./App.css";
 import ReactDDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './Layout';
-import Home from './pages/Home';
-import Team from './pages/Team';
-import Vision from './pages/Vision';
-import Custom from './pages/Custom';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+import Team from "./pages/Team/index";
+import Vision from "./pages/Vision";
+import Custom from "./pages/Custom";
 
 export default function App() {
-
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path = "/" element={<Layout/>}>
-          <Route index element={<Home/>}/>
-          <Route path="vision" element={<Vision/>}/>
-          <Route path="team" element={<Team/>}/>
-          <Route path="custom" element={<Custom/>}/>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="vision" element={<Vision />} />
+          <Route path="team" element={<Team />} />
+          <Route path="custom" element={<Custom />} />
         </Route>
       </Routes>
     </BrowserRouter>
-
   );
 }
